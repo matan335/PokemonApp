eba24b23fc65953175db75f15e529bb1273c581e
Matan <matancoder114@gmail.com>
2018-09-27 21:43:33 +0300

    fix bug not loading

--- src/main.js
+++ src/main.js
@@ -5,13 +5,10 @@ import store from './store'
 import './registerServiceWorker'
 import service from './services/pokemon.service'
 
-<<<<<<< HEAD
 service.query()
   .then(pokemons => console.log(pokemons))
   .catch(err => console.log(err))
 
-=======
->>>>>>> 1adf7ef2f17feab5bac29572e3fbe20dd8baa9e0
 Vue.config.productionTip = false
 
 new Vue({
